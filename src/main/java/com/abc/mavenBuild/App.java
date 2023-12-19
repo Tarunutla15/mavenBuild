4d62f5ba95f8ac2c7637e293264648dd232f7ac3
tarun <tarun@gmail.com>
2023-12-19 12:34:49 +0530

    code updated

--- src/main/java/com/abc/mavenBuild/App.java
+++ src/main/java/com/abc/mavenBuild/App.java
@@ -1,13 +1,29 @@
 package com.abc.mavenBuild;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int l[]= {1,4,7,9,14};
+        int key=91,ind=-1;
+        
+        int i=0,j=l.length-1;
+        int mid=(i+j)/2;
+        if (key<l[mid]){
+        	for(int k=0;k<mid;k++) {
+        		if (l[k]==key){
+        			ind=k;
+        		}
+        	}
+        }
+        else {
+        	for(int k=mid;k<j;k++) {
+        		if (key==l[k]) {
+        			ind=k;
+        		}
+        	}
+        }
+        System.out.println("index of searched number "+ind);
     }
 }
+
